e000e3e10fcf8e406bdd61b9076d96af113912b2
markfialko <markfialkofialko@gmail.com>
2023-10-29 20:24:50 +0500

    feat: login and registration pages

--- src/modules/account/views/Login.vue
+++ src/modules/account/views/Login.vue
@@ -1,7 +1,144 @@
 <template>
-  <div>LOGIN</div>
+  <div class="container login-container">
+    <form class="login-form">
+      <p class="login-form__title">Постоянный покупатель</p>
+      <AppInput label="Электронная почта" placeholder="yourname@mail.com" v-model="email" />
+      <AppInput label="Ваш пароль" placeholder="От 5 и более символов" v-model="password" />
+      <AppButton>Создать аккаунт</AppButton>
+    </form>
+    <div class="login-info">
+      <div class="login-info__container">
+        <p class="login-title">Создайте аккаунт</p>
+        <p class="login-subtitle">
+          Делайте покупки быстро и удобно, отслеживайте статус заказа и получайте скидки.
+        </p>
+        <AppButton
+          class="login-info__button"
+          variant="outline"
+          type="link"
+          :to="Routes.REGISTRATION"
+          >Зарегистрироваться
+        </AppButton>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import AppInput from '@/shared/ui-kit/app-input/AppInput.vue'
+import AppButton from '@/shared/ui-kit/app-button/AppButton.vue'
 
-<style scoped></style>
+import giftIcon from '@/modules/account/icons/giftIcon.svg'
+import garantyIcon from '@/modules/account/icons/garantyIcon.svg'
+import paymentIcon from '@/modules/account/icons/paymentIcon.svg'
+import { Routes } from '@/app/router/types'
+
+const fullName = ref('')
+const email = ref('')
+
+const password = ref('')
+const passwordRepeat = ref('')
+
+const REGISTER_BONUSES = [
+  {
+    title: 'Возвращаем 10% бонусами',
+    description:
+      'При заказе на сумму от 3500 руб. и весе до 3 кг. предоставляется бесплатная доставка по всей Россиию',
+    icon: giftIcon
+  },
+  {
+    title: 'Гарантия качества',
+    description:
+      'При заказе на сумму от 3500 руб. и весе до 3 кг. предоставляется бесплатная доставка по всей Россиию',
+    icon: garantyIcon
+  },
+  {
+    title: 'Удобные способы оплаты',
+    description:
+      'При заказе на сумму от 3500 руб. и весе до 3 кг. предоставляется бесплатная доставка по всей Россиию',
+    icon: paymentIcon
+  }
+]
+</script>
+
+<style lang="scss">
+.login-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  @media (max-width: $md) {
+    grid-template-columns: 1fr;
+  }
+
+  .login-title {
+    font-size: 28px;
+    font-weight: 700;
+    margin-bottom: 12px;
+    @media (max-width: $md) {
+      font-size: 24px;
+    }
+  }
+
+  .login-subtitle {
+    font-size: 16px;
+    flex: 1 1 auto;
+    margin-bottom: 40px;
+  }
+
+  .login-bonuses {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    @media (max-width: $md) {
+      gap: 20px;
+    }
+  }
+}
+
+.login-info {
+  padding: 40px 55px;
+  display: flex;
+  flex-direction: column;
+  background: rgba($accent-color, 0.2);
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  @media (max-width: $lg) {
+    padding: 20px;
+  }
+
+  &__container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  &__button {
+    width: 100%;
+    max-width: 260px;
+  }
+}
+
+.login-form {
+  background: #ffffff;
+  padding: 40px 55px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  @media (max-width: $lg) {
+    padding: 20px;
+  }
+
+  &__title {
+    text-align: center;
+    margin-bottom: 80px;
+    font-size: 28px;
+    font-weight: 700;
+    @media (max-width: $md) {
+      margin-bottom: 20px;
+      font-size: 24px;
+    }
+  }
+}
+</style>
--- src/modules/account/views/Registration.vue
+++ src/modules/account/views/Registration.vue
@@ -1,7 +1,162 @@
 <template>
-  <div>REGISTRATION</div>
+  <div class="container register-container">
+    <div class="register-info">
+      <p class="register-title">Регистрация аккаунта</p>
+      <p class="register-subtitle">
+        Делайте покупки быстро и удобно, отслеживайте статус заказа и получайте скидки.
+      </p>
+      <div class="register-bonuses">
+        <div class="register-bonus" v-for="bonus in REGISTER_BONUSES" :key="bonus.title">
+          <img class="register-bonus__icon" :src="bonus.icon" :alt="bonus.title" />
+          <p class="register-bonus__title">{{ bonus.title }}</p>
+          <span class="register-bonus__description">{{ bonus.description }}</span>
+        </div>
+      </div>
+    </div>
+    <form class="register-form">
+      <AppInput label="Ваше имя и фаимилия" placeholder="Владимир Иванов" v-model="fullName" />
+      <AppInput label="Электронная почта" placeholder="yourname@mail.com" v-model="email" />
+      <div class="register-form__passwords">
+        <AppInput label="Пароль для входа" placeholder="От 5 и более символов" v-model="password" />
+        <AppInput
+          label="Подтвердите пароль"
+          placeholder="Повторите пароль"
+          v-model="passwordRepeat"
+        />
+      </div>
+      <AppButton>Создать аккаунт</AppButton>
+      <p class="register-form__bottom">
+        Уже есть профиль?
+        <router-link class="register-form__link" :to="Routes.LOGIN">Войти в аккаунт</router-link>
+      </p>
+    </form>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import AppInput from '@/shared/ui-kit/app-input/AppInput.vue'
+import AppButton from '@/shared/ui-kit/app-button/AppButton.vue'
 
-<style scoped></style>
+import giftIcon from '@/modules/account/icons/giftIcon.svg'
+import garantyIcon from '@/modules/account/icons/garantyIcon.svg'
+import paymentIcon from '@/modules/account/icons/paymentIcon.svg'
+import { Routes } from '@/app/router/types'
+
+const fullName = ref('')
+const email = ref('')
+
+const password = ref('')
+const passwordRepeat = ref('')
+
+const REGISTER_BONUSES = [
+  {
+    title: 'Возвращаем 10% бонусами',
+    description:
+      'При заказе на сумму от 3500 руб. и весе до 3 кг. предоставляется бесплатная доставка по всей Россиию',
+    icon: giftIcon
+  },
+  {
+    title: 'Гарантия качества',
+    description:
+      'При заказе на сумму от 3500 руб. и весе до 3 кг. предоставляется бесплатная доставка по всей Россиию',
+    icon: garantyIcon
+  },
+  {
+    title: 'Удобные способы оплаты',
+    description:
+      'При заказе на сумму от 3500 руб. и весе до 3 кг. предоставляется бесплатная доставка по всей Россиию',
+    icon: paymentIcon
+  }
+]
+</script>
+
+<style lang="scss">
+.register-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  @media (max-width: $md) {
+    grid-template-columns: 1fr;
+  }
+
+  .register-title {
+    font-size: 28px;
+    font-weight: 700;
+    margin-bottom: 12px;
+    @media (max-width: $md) {
+      font-size: 24px;
+    }
+  }
+
+  .register-subtitle {
+    font-size: 16px;
+    flex: 1 1 auto;
+    margin-bottom: 40px;
+  }
+
+  .register-bonuses {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    @media (max-width: $md) {
+      gap: 20px;
+    }
+  }
+
+  .register-bonus {
+  }
+
+  .register-bonus__icon {
+    margin-bottom: 8px;
+  }
+
+  .register-bonus__title {
+    font-size: 16px;
+    font-weight: 700;
+    margin-bottom: 4px;
+  }
+
+  .register-bonus__description {
+  }
+}
+
+.register-info {
+  padding: 40px 55px;
+  display: flex;
+  flex-direction: column;
+  background: rgba($accent-color, 0.2);
+  @media (max-width: $lg) {
+    padding: 20px;
+  }
+}
+
+.register-form {
+  background: #ffffff;
+  padding: 40px 55px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  @media (max-width: $lg) {
+    padding: 20px;
+  }
+
+  &__passwords {
+    display: flex;
+    gap: 20px;
+    width: 100%;
+    flex: 1 1 auto;
+    @media (max-width: $lg) {
+      flex-direction: column;
+    }
+  }
+
+  &__bottom {
+    align-self: center;
+
+    .register-form__link {
+      color: $primary-color;
+    }
+  }
+}
+</style>
