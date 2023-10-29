f72edc5d42f41eaffb803d4dbf13436fbb3bcf9a
markfialko <markfialkofialko@gmail.com>
2023-10-29 20:23:23 +0500

    feat: add icons

---
+++ src/shared/components/Logo.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-link to="/" class="logo" />
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.logo {
+  width: 40px;
+  height: 40px;
+  border-radius: 5px;
+  background: $primary-color;
+}
+</style>
