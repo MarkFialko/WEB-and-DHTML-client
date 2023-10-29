e588dcd13e7244f3d2d787fdbb8dd7a169fef58d
markfialko <markfialkofialko@gmail.com>
2023-10-29 20:24:14 +0500

    feat: app button component

---
+++ src/shared/ui-kit/app-button/AppButton.vue
@@ -0,0 +1,82 @@
+<template>
+  <button
+    v-bind="$attrs"
+    v-if="type === 'button'"
+    @click.prevent
+    class="app-button"
+    :class="[variant]"
+    :disabled="loading || disabled"
+  >
+    <template v-if="loading">
+      <Spinner />
+    </template>
+    <template v-else>
+      <slot />
+    </template>
+  </button>
+  <router-link
+    v-bind="$attrs"
+    v-if="type === 'link'"
+    :to="to"
+    class="app-button"
+    :class="[variant]"
+    :disabled="loading || disabled"
+  >
+    <template v-if="loading">
+      <Spinner />
+    </template>
+    <template v-else>
+      <slot />
+    </template>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import Spinner from '@/shared/components/Spinner.vue'
+
+interface Props {
+  type: 'link' | 'button'
+  to?: string
+  variant: 'common' | 'outline'
+  disabled?: boolean
+  loading?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  type: 'button',
+  variant: 'common',
+  disabled: false,
+  loading: false
+})
+</script>
+
+<style lang="scss">
+.app-button {
+  cursor: pointer;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  background: $primary-color;
+  color: #ffffff;
+  border: none;
+
+  &[disabled=true] {
+    background: rgba($primary-color, 0.5);
+    pointer-events: none;
+    cursor: default;
+  }
+
+  &.outline {
+    background: transparent;
+    border: 1px solid $primary-color;
+    color: $primary-color;
+
+    &[disabled=true] {
+      background: transparent;
+      border: 1px solid rgba($primary-color, 0.5);
+    }
+  }
+}
+</style>
