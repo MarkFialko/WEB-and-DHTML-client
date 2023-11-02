2177387e3f8554f37d416f53d9e8d4c8a4f76ba9
markfialko <markfialkofialko@gmail.com>
2023-11-03 00:38:31 +0500

    feat: страница аккаунта пользователя

---
+++ src/modules/account/Account.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container account-container">
+    <div class="account-activities">
+      <AppButton @click="logout">Выйти</AppButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppButton from '@/shared/ui-kit/app-button/AppButton.vue'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+
+const store = useStore()
+const router = useRouter()
+const logout = async () => {
+  await store.dispatch('account/logout')
+  router.push('/')
+}
+</script>
+
+<style scoped lang="scss">
+.account {
+  &-container {
+    width: 100%;
+  }
+
+  &-activities {
+  }
+}
+</style>
