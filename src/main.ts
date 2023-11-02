1c94cb83f48f8ec2d8874f3d852c4202987e8853
markfialko <markfialkofialko@gmail.com>
2023-11-03 00:41:43 +0500

    feat: подключение стора

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
 
 import '@/shared/styles/styles.scss'
 
 import App from '@/app/App.vue'
 import router from '@/app/router'
+import store from '@/app/store'
 
 const app = createApp(App)
 
-app.use(createPinia())
+// app.use(createPinia())
 app.use(router)
+app.use(store)
 app.mount('#app')
