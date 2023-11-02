d7d26a15286645edbf7b1bd15aab543b83dd0465
markfialko <markfialkofialko@gmail.com>
2023-11-03 00:40:20 +0500

    feat: router

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,12 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from '@/app/router/routes'
+import store from '@/app/store'
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
-router.beforeEach(async (to, from, next) => {
+router.beforeEach((to, from, next) => {
+  store.dispatch('account/getMe').catch(() => {
+    if (
+      to.matched.some((record) => record.meta.authRequired) &&
+      !store.getters['account/getIsAuth']
+    ) {
+      router.push('/')
+    }
+  })
+
   next()
 })
 
