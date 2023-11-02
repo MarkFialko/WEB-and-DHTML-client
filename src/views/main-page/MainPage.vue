8e593ef35467564c412628367a3ee11dececfa9f
markfialko <markfialkofialko@gmail.com>
2023-11-03 00:40:42 +0500

    feat: осноная страница

--- src/views/main-page/MainPage.vue
+++ src/views/main-page/MainPage.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="container main-page">
     <div class="dishes-block">
-      <DishCard />
-      <DishCard />
-      <DishCard />
-      <DishCard />
-      <DishCard />
+      <DishCard v-for='dish in dishes' :key='dish.id'
+                :name='dish.name'
+                :price='dish.price'
+                :description='dish.description'
+                :id='dish._id'
+      />
+
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import DishCard from '@/shared/components/DishCard.vue'
+import { onMounted, ref } from 'vue'
+import { DishApi } from '@/api/Dish.api'
+
+const loading = ref(false)
+
+const dishes = ref([])
+
+onMounted(()=> {
+  getDishes()
+})
+
+const getDishes = async () => {
+  loading.value = true
+  try {
+      const response = await DishApi.getAll()
+    console.log(response.dishes)
+    dishes.value = response.dishes
+  } catch (e) {
+
+  }
+}
+
 </script>
 
 <style lang="scss">
